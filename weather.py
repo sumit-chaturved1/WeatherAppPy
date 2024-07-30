78cfdf7bce6a6118273fc65b5f388d1a59545211
Sumit <94129378+sumit-chaturved1@users.noreply.github.com>
2024-07-31 01:53:58 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 def get_current_weather(city="Delhi"):
-  request_url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("API_KEY")}&units=metric"
+  request_url= f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric"
   weather_data = requests.get(request_url).json()
   return weather_data
 
